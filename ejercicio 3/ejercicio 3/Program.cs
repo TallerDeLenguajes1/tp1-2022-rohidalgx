36cfcaba1b38c5b02892859f8d2b52c10df18334
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-08-26 19:01:57 -0300

    primer commit

---
+++ ejercicio 3/ejercicio 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.WriteLine("\nHolis");
+try
+{
+    Console.WriteLine("\n Ingrese la cantidad de kilometros conducidos");
+    int kilometros = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n Ingrese la cantidad de litros de nafta:");
+    int nafta = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n Kilometros/litro:" + kilometros/nafta);
+}
+catch
+{
+    Console.WriteLine("\n No se puede dividir en 0 ni se aceptan letras");
+}
---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+
+Console.WriteLine("\nHolis");
+try
+{
+    Console.WriteLine("\n Ingrese un numero entero:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n Ingrese otro numero entero:");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n Resultado de la division:" + num1/num2);
+}
+catch
+{
+    Console.WriteLine("\n No se puede dividir en 0");
+}
